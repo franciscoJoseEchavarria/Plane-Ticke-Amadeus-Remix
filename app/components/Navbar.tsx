2f393525c72893d14444702d7dd33b117b76693a
luz Aidee <luz-aiderc@hotmail.com>
2025-03-25 17:22:36 -0500

    add footer, HomePage and update Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,18 @@
+import { Link } from "@remix-run/react";
+
 export default function Navbar() {
     return (
         <nav className="bg-white border-b border-gray-200">
-            <div className="container ml-14 flex items-center justify-start pb-6 pt-6">
+            <div className="container flex items-center justify-start py-6 pl-14">
                 {/* Logo */}
                 <div className="flex items-center">
                     <img src="amadeus-logo.png" alt="Logo Amadeus" className="h-4 w-auto" />
                 </div>
                 {/* Navigation Links */}
                 <ul className="flex space-x-16 text-gray-700 font-medium ml-12">
-                    <li className="hover:text-blue-500 cursor-pointer">Inicio</li>
+                    <li><Link to="/" className="hover:text-blue-500">Inicio</Link></li>
                     <li className="hover:text-blue-500 cursor-pointer">Reporte</li>
-                    <li className="hover:text-blue-500 cursor-pointer">Amadeus</li>
+                    <li><a href="https://amadeus.com/es" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">Amadeus</a></li>
                 </ul>
             </div>
         </nav>
